b70140954e703d1c697da4fb441dd2fe85c42497
HECTOR DAVID MASSO BERMUDEZ <hectormasso@hotmail.com>
2020-05-28 20:11:57 -0500

    UserController, viewModel, Helpers

---
+++ GameON.Common/Models/UserResponse.cs
@@ -0,0 +1,27 @@
+﻿using GameON.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameON.Common.Models
+{
+    public class UserResponse
+    {
+        public string Id { get; set; }
+
+        
+
+        public string Email { get; set; }
+
+        public string Document { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public UserType UserType { get; set; }
+
+    }
+}
---
+++ GameON.Web/Helpers/IMailHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameON.Common.Models;
+
+namespace GameON.Web.Helpers
+{
+    
+        public interface IMailHelper
+        {
+            Response SendMail(string to, string subject, string body);
+        }
+    
+}
